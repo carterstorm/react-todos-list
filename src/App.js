062d6ce603e2372eafcfce479fa4ee37d99b35ae
Sebastian Komorowski <sebastiano.komo@hotmail.com>
2022-03-11 20:24:08 +0100

    created html structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 function App() {
   return (
-   <div></div>
-  );
+    <main className="container">
+      <h1>To-do list</h1>
+      <section className="section">
+        <h2 className="section__header">Add a new task</h2>
+        <div className="section__body">
+          <form className="form">
+            <input className="form__input" type="text" placeholder="Type here what are you gonna do?"/>
+            <button className="button">Add task</button>
+          </form>
+        </div>
+      </section>
+      <section className="section">
+        <div className="section__container">
+          <h2 className="section__header section__header--offborder">What you need to do:</h2>
+            <div className="section__buttons"></div>
+        </div>
+        <div className="section__body">
+          <ul className="list">
+          </ul>
+        </div>
+      </section>
+    </main>
+    );
 }
 
 export default App;
