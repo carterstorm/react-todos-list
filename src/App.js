db50619e0d8992ca115784fdb2d02d3312610c9b
Sebastian Komorowski <sebastiano.komo@hotmail.com>
2022-03-13 14:19:16 +0100

    Added Container component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import { Form } from "./Form";
 import { Buttons } from "./Buttons";
 import { List } from "./List";
 import { Section } from "./Section";
+import { Container } from "./Container";
 
 function App() {
 
   let tasks = [
     {
-      id: 1, content: "Example 1", done: false,
+      id: 1, content: "Example 1", done: true,
     },
     {
       id: 2, content: "Example 2", done: false
@@ -17,7 +18,7 @@ function App() {
   let hideButtons = false;
 
   return (
-    <main className="container">
+    <Container>
       <h1>To-do list</h1>
       <Section
         title = {`Add a new task`}
@@ -28,8 +29,8 @@ function App() {
         header={<Buttons tasks={tasks} hideButtons={hideButtons}/>}
         body = {<List tasks={tasks} hideButtons={hideButtons}/>}
       />
-    </main>
-    );
+    </Container>
+  );
 }
 
 export default App;
