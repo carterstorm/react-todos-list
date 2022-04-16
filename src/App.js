472e6e312fb147808236361c74b8fdeb193a617c
Sebastian Komorowski <sebastiano.komo@hotmail.com>
2022-04-16 13:11:59 +0200

    Added tasks to localeStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { Form } from "./Form";
 import { Buttons } from "./Buttons";
 import { List } from "./List";
@@ -7,11 +7,14 @@ import { Container } from "./Container";
 
 function App() {
 
-  const [tasks, setTasks] = useState([
-    {id: 1, content: "Example 1", done: true},
-    {id: 2, content: "Example 2", done: false},
-  ]);
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks"))
+  );
   
+  useEffect(()=> {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+  }, [tasks]);
+
   const [hideDone, setHideDone] = useState(false);
 
   const toogleHideDone = () => {
