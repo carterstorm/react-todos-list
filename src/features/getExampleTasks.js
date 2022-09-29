96dfef06d0fd56a6f165491550e4c512d0fdd2cf
Sebastian Komorowski <sebastiano.komo@hotmail.com>
2022-09-29 19:44:38 +0200

    fetch data from file

---
+++ src/features/getExampleTasks.js
@@ -0,0 +1,10 @@
+export const getExampleTasks = async () => {
+
+    const response = await fetch("./exampleTasks.json");
+
+    if (!response.ok) {
+        new Error(response.statusText);
+    }
+
+    return await response.json();
+};
