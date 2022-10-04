1946b2cbeede39f01ac54919c6ba5fbad369eecb
Sebastian Komorowski <sebastiano.komo@hotmail.com>
2022-10-04 21:02:19 +0200

    Added first element to Navigation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,21 @@
 import React from "react";
+import { HashRouter, Link, Switch, Route } from "react-router-dom";
 import Tasks from "../src/features/Tasks";
 
-export const App = () => <Tasks />;
+export default () => (
+    <HashRouter>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/tasks">Tasks</Link>
+                </li>
+            </ul>
+
+            <Switch>
+                <Route path="/tasks" >
+                    <Tasks />
+                </Route>
+            </Switch>
+        </nav>
+    </HashRouter>
+);
