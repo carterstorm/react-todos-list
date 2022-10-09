cbfd17b243f2a87f3310be26fc172e20da6ff2a4
Sebastian Komorowski <sebastiano.komo@hotmail.com>
2022-10-09 08:50:36 +0200

    Styled Navigation

--- src/features/Navigation/styled.js
+++ src/features/Navigation/styled.js
@@ -2,10 +2,19 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 export const List = styled.ul`
+    display: flex;
+    justify-content: center;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    background-color: ${({ theme }) => theme.colors.teal};
 `;
 
 export const Item = styled.li`
+    margin: 20px;
 `;
 
 export const StyledNavLink = styled(NavLink)`
+    color: ${({ theme }) => theme.colors.white};
+    text-decoration: none;
 `;
