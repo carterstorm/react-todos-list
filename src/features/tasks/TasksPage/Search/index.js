3160f89f50ab270ecd7fe8eaf4ec90afb753a8b7
Sebastian Komorowski <sebastiano.komo@hotmail.com>
2022-10-10 21:22:59 +0200

    created handler function for URLSearchParams

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -8,11 +8,20 @@ export const Search = () => {
     const searchParams = new URLSearchParams(location.search);
     const query = searchParams.get("search");
 
+    const onInputChange = ({ target }) => {
+        if (target.value.trim() === "") {
+            searchParams.delete("search");
+        } else {
+            searchParams.set("search", target.value);
+        };
+    };
+
     return (
         <Wrapper>
             <Input
                 placeholder="Type here to find a task"
                 value={query || ""}
+                onChange={onInputChange}
             />
         </Wrapper>
     );
