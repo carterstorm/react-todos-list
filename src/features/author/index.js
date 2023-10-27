99b55962257a4683c42262ad243891c1faaf16cf
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-10-27 19:51:36 +0200

    styled AuthorContainer and AuthorParagraphs

--- src/features/author/index.js
+++ src/features/author/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Section } from "../../common/Section";
 import { Container } from "../../common/Container";
+import { AuthorContainer, Paragraph } from "./styled";
 
 export const Author = () => (
     <Container>
@@ -8,20 +9,21 @@ export const Author = () => (
         <Section
             title={`Sebastian Komorowski`}
             body={
-                <>
-                    <p> Hi, I'm Sebastian. I live in Warsaw. I have been learning Frontend technology
+                <AuthorContainer>
+                    <Paragraph>
+                        Hi, I'm Sebastian. I live in Warsaw. I have been learning Frontend technology
                         for some time and... it gives me a lot of pleasure. In the nearest future I would like
                         to work as a Junior Frontend Developer with React.🤙
-                    </p>
-                    <p>
+                    </Paragraph>
+                    <Paragraph>
                         My biggest passion is traveling. I like the most these spontaneous trips on Friday afternoons.
                         In my spare time, I like to ride a bike, walk in parks, play bowling and from time to time go to the theater. 🛫 🚲 🌳 🎳 🎭
-                    </p>
-                    <p>
+                    </Paragraph>
+                    <Paragraph>
                         I like to fix different kind of things. I'm a born handyman, so in the future, I would
                         like to build a small wooden house only by my own hands! 🏠
-                    </p>
-                </>
+                    </Paragraph>
+                </AuthorContainer>
             }
         />
     </Container>
---
+++ src/features/author/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const AuthorContainer = styled.div`
+    display: grid;
+    grid-gap: 20px;
+`;
+
+export const Paragraph = styled.span`
+    line-height: 1.6;
+    font-weight: 400;
+`;
